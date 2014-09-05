b1eb39f90567fe58c47aec678e964a6d6c00724d
Adeleke <adeleketogun@gmail.com>
2014-09-05 18:58:22 +0100

    Working Back end

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 //Computer selects a random number from 0-100
 var generated_number = Math.round(Math.random()*100);
 
-//Setting previous guess to zero
-var previous_guess = 0
+// //Setting previous guess to zero
+var previous_guess = 0;
 
-
- function game () {
+var numGuesses= 0;
+alert(generated_number);
+function game () {
 
 	//Prompt's User to make a guess
 	var user_guess = prompt("Guess the number");
@@ -14,40 +15,56 @@ var previous_guess = 0
 	var check_validity = function (user_guess) {
 
 		if (isNaN(user_guess)) {
-			alert("You have to input a number from 0 to 100 to move on")
+			alert("You have to input a number from 0 to 100 to move on");
 		}
 
 		else if (user_guess < 0) {
-			alert("The number you entered is less than zero, your input must span between 0 to 100 to continue")
+			alert("The number you entered is less than zero, your input must span between 0 to 100 to continue");
 		}
 
 		else if (user_guess > 100) {
-			alert("The number you entered is greater than 100, your input must span between 0 to 100 to continue")
+			alert("The number you entered is greater than 100, your input must span between 0 to 100 to continue");
 		}
 
+		else {
+			return true;
+		}
+
+		return false;
+
 	}
 
-	check_validity(user_guess);
-
-	//test the level of correctness of the guess
- 	var correct_o_meter = (user_guess / generated_number) * 100;
-
- 	 if (user_guess === 100) {
-		prompt("Bull's eye, you win")
+	if (!check_validity(user_guess)) {
+		game();
 	}
 
-	else {
- 			if (user_guess > correct_o_meter) {
- 				alert("You are getting Hotter")
- 			}
+ 	if (parseInt(user_guess) === generated_number) {
+ 		alert("Bull's Eye");
+ 	} else {
+ 		if(numGuesses === 0) {
+ 			alert("You are Hot (hey sexy)");
 
-			else if (user_guess < correct_o_meter) {
- 				alert("You are getting colder")
- 			} 
- 		}
+		} 
+		else {
+			var previous_range = Math.abs(generated_number - previous_guess);
+	 		var current_range = Math.abs(generated_number - user_guess);
 
- 	previous_guess = guess;
+			if (previous_range < current_range) {
+				alert("You are getting colder")
+			} else if (previous_range > current_range) {
+				alert("You are getting hotter")
+			}
 
- }
- game();
 
+ 			
+		}
+		previous_guess = user_guess
+ 		
+ 		numGuesses++;
+		
+		game();
+ 	}
+
+}
+
+game();
